82be63befb7dcec07c8aee5dbbc4dececc523000
Higas01 <higor.porangaba123@hotmail.com>
2023-05-08 13:49:47 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import FirstSection from "./components/FirstSection";
 import SecondSection from "./components/SecondSection";
 import ThreeSection from "./components/ThreeSection";
 import FourSection from "./components/FourSection";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <SecondSection />
       <ThreeSection />
       <FourSection />
+      <Footer />
     </>
   );
 }
